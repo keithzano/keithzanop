82bc4bce770350ffb532ec824f69ff2b64c13d44
keithzano <keithzano@gmail.com>
2025-05-09 20:51:47 +0200

    Change copy

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -8,15 +8,15 @@ export const Hero: FC = () => {
         <h1 className="text-foreground space-y-3 text-xl lg:max-w-3xl">
           Hi I am Keith Zano <br />
           <span className="text-foreground font-tight max-w-3xl text-[clamp(40px,8vw,80px)] leading-none font-bold">
-            I Build web stuff in exchange for money.
+            I build web stuff in exchange for money
           </span>
         </h1>
         <p className="text-foreground max-w-lg leading-relaxed font-medium md:text-xl">
-          I am a software developer who has been turning caffein in code since
-          2020. Will build your website if you promise not to ask for ‘something
-          like Airbnb but simpler’. “I search Stack Overflow like it’s my job
-          (wait, it is). Then I stitch together answers until the code works. ✨
-          Art ✨”
+          I'm a software developer who’s been turning caffeine into code since
+          2020. I'll build your website—as long as you don’t ask for “something
+          like Airbnb but simpler.” I treat Stack Overflow like a second brain,
+          then duct-tape answers together until something shippable emerges. ✨
+          It's not a bug, it's a feature ✨
         </p>
         <div className="space-x-6">
           <Button size={"lg"} variant={"outline"}>
